01d0f69bc608ee75fc70611d464ef6d910ee4b49
ayushudaydubey <102001ayushdubey@gmail.com>
2024-10-03 13:08:30 +0530

    spotify app

--- scrollTrigger/app.js
+++ scrollTrigger/app.js
@@ -13,7 +13,7 @@ gsap.to(".page2 h2",{
     }
 })
 
-var obj ={name : "ayush",
+/*var obj ={name : "ayush",
     age :22
 }
 var copyobj ={...obj}
@@ -25,4 +25,29 @@ function abcd ( a){
 
 abcd(function (){
     console.log("hello")
+})*/
+
+/*async function abcd() {
+    let raw = await fetch(`https://randomuser.me/api/`)
+    let ans = await raw.json();
+    console.log (ans);
+    
+}*/
+
+var ans = new Promise((resolve, reject) => {
+    
+    return resolve("welcome home")
 })
+var p2 = ans.then(function(data ){
+    console.log(data) 
+    return new Promise((resolve, reject) => {
+        return resolve("take shower")
+    })
+})
+var p3 = p2.then(function(data ){
+    console.log(data)
+    return new Promise((resolve, reject) => {
+        console.log("take meal")
+    })
+})
+console.log(p3)
